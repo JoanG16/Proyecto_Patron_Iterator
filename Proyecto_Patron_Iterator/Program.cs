94a0f639da92e4055264c69d4989f6f1135babc7
JoanG16 <94708855+JoanG16@users.noreply.github.com>
2021-12-16 09:47:25 -0500

    Update Program.cs

--- Proyecto_Patron_Iterator/Program.cs
+++ Proyecto_Patron_Iterator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Proyecto_Patron_Iterator
 {
@@ -6,8 +6,12 @@ namespace Proyecto_Patron_Iterator
     {
         static void Main(string[] args)
         {
-
+            //tenemos la instancia de la clase CContenedor
             CContenedor date = new CContenedor();
+
+         //hacemos uso del foreach
+         //que va a pedir a CContenedor el enumerador, luego de eso va a ir invocando el reset para colocarlo al icnio del contenedor,
+         //el movenext para ver si hay o no elementos, en el momento que ya no hay as elementos en esa coleccion entonces el foreach finaliza 
             foreach (string variable in date)
             {
                 Console.WriteLine(variable);
@@ -15,3 +19,4 @@ namespace Proyecto_Patron_Iterator
         }
     }
 }
+
