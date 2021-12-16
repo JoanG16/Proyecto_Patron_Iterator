d5c2b588059400a345730095ced48062ad649ece
JOAN JOB GALLARDO ALCIVAR <e1313197400@live.uleam.edu.ec>
2021-12-16 09:08:44 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Patron_Iterator/CContenedor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Patron_Iterator
+{
+    class CContenedor : IEnumerable
+    {
+        private string[] elementos = new string[5];
+        public CContenedor()
+        {
+            elementos = new string[] { "Joan", "Juan", "Wilson", "Mateo", "Miriam" };
+
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (new ContenedorEnum(elementos));
+
+        }
+    }
+    class ContenedorEnum : IEnumerator
+    {
+        public string[] array;
+        private int posicion = -1;
+        public ContenedorEnum(string[] pArray)
+        {
+            array = pArray;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return array[posicion];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            posicion++;
+            if (posicion < array.Length)
+                return true;
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            posicion = -1;
+        }
+    }
+}
---
+++ Proyecto_Patron_Iterator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Proyecto_Patron_Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CContenedor date = new CContenedor();
+            foreach (string variable in date)
+            {
+                Console.WriteLine(variable);
+            }
+        }
+    }
+}
